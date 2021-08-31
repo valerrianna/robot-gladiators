6592e28aab5dd9f63fe5e2c5e6c794b844c75f60
Valerie <valerie.tram.anh.n@gmail.com>
2021-08-30 22:03:12 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,45 +7,42 @@ var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var randomNumber = function(min,max) {
+  var value = Math.floor(Math.random()*max - min + 1) + min;
+  return value;
+};
+
 var fight = function(enemyName) {
   while(playerHealth > 0 && enemyHealth > 0) {
-    // Alert players that they are starting the round
       var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
-      // if player choses to fight, then fight
   if (promptFight === "fight" || promptFight === "FIGHT") {
-      // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack-3,playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
       );
-    
-      // check enemy's health
       if (enemyHealth <= 0) {
         window.alert(enemyName + " has died!");
         break;
       } else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.");
       }
-    
-      // remove player's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack-3, enemyAttack);
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
       );
-    
-      // check player's health
       if (playerHealth <= 0) {
         window.alert(playerName + " has died!");
         break;
       } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
       }
-      // if player choses to skip
     } else if (promptFight === "skip" || promptFight === "SKIP") {
       var confirmSkip = window.confirm("Are you sure you'd like to quit?");
           if (confirmSkip) {
               window.alert(playerName + " has decided to skip this fight. Goodbye!");
-              playerMoney = playerMoney - 10;
+              playerMoney = Math.max(0,playerMoney -10);
               console.log("playerMoney",playerMoney);
               break;
           }
@@ -66,7 +63,7 @@ var startGame = function() {
     if (playerHealth > 0) {
       window.alert("Welcome to Robot Gladiators! Round " + (i+1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40,60);
       fight(pickedEnemyName);
     }
     if (playerHealth > 0 && i < enemyNames.length -1) {
